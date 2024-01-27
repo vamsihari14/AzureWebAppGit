163035d3cf959066e3a4ffd56abc28764aba56bb
vamsi k s <vamsihari14@gmail.com>
2024-01-27 14:10:05 +0530

    Add project files.

---
+++ AzureWebApps/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureWebApps.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }    
+        public int Quantity { get; set; }
+    }
+}
---
+++ AzureWebApps/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using AzureWebApps.Models;
+using AzureWebApps.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureWebApps.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+
+        }
+    }
+}
